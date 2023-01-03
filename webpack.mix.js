f7d00a85cdddc925ba0970072abd470def33361e
MalcoMarco <minayamalco@gmail.com>
2023-01-02 20:57:20 -0500

    svg flujos

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
-//mix.js('src/app.js', 'dist').setPublicPath('dist').version();
+mix.js('src/flujo-svg/app.js', 'flujo-svg').setPublicPath('flujo-svg').version();
 //mix.js('src/app.js', 'dist').vue({ version: 2 });
-mix.js('src/formulario.js', 'dist').vue({ version: 2 });
+//mix.js('src/formulario.js', 'dist').vue({ version: 2 });
